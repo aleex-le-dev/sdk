049ea14a03c86e6f6d16ef6cb8f4f2afdfda9eaf
Aleexj <jalias59@outlook.fr>
2025-07-10 15:41:21 +0200

    Ajout de la prise en charge de la sélection d'images à partir de la bibliothèque pour les marqueurs sur la carte, avec mise à jour des dépendances pour inclure expo-image-picker. Modification du composant MarkerItem pour afficher l'image sélectionnée.

--- components/map/Map.js
+++ components/map/Map.js
@@ -2,8 +2,12 @@ import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useState } from "react";
 import MarkerItem from "./MarkerItem";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Map() {
+  const [libraryStatus, requestLibraryPermission] =
+    ImagePicker.useMediaLibraryPermissions();
+
   const initialRegion = {
     latitude: 50.552690015432,
     longitude: 2.418493974217,
@@ -12,16 +16,29 @@ export default function Map() {
   };
 
   const [markers, setMarkers] = useState([
-    {
-      coordinate: { latitude: 50.552690015432, longitude: 2.418493974217 },
-      isDragging: false,
-    },
-
+    // {
+    //   coordinate: { latitude: 50.552690015432, longitude: 2.418493974217 },
+    //   isDragging: false,
+    //   imageSource: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUqNqnr8-J5enuQU81PuPhc_qIMSi9cIDXlQ&s"
+    // },
   ]);
 
-  const addMarker = (event) => {
-    const { coordinate } = event.nativeEvent;
-    setMarkers((current) => [...current, { coordinate, isDragging: false }]);
+  const addMarker = async (event) => {
+    event.persist();
+    if (!libraryStatus?.granted) {
+      await requestLibraryPermission();
+    }
+
+    if (libraryStatus?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        quality: 0.5,
+      });
+
+      if (!result.canceled) {
+        const { coordinate } = event.nativeEvent;
+        setMarkers((current) => [...current, { coordinate, isDragging: false, imageSource:result.assets[0].uri}]);
+      }
+    }
   };
 
   const dragstart = (index) => {
@@ -29,8 +46,7 @@ export default function Map() {
     MarkersCopy[index].isDragging = true;
     setMarkers(MarkersCopy);
   };
-  
-  
+
   const endStart = (index) => {
     const MarkersCopy = [...markers];
     MarkersCopy[index].isDragging = false;
@@ -50,9 +66,8 @@ export default function Map() {
           draggable
           stopPropagation
           onDragStart={() => dragstart(index)}
-          onDragEnd={() => endStart(index)}
-        >
-          <MarkerItem isDragging={marker.isDragging} />
+          onDragEnd={() => endStart(index)}>
+          <MarkerItem isDragging={marker.isDragging} imageSource={marker.imageSource} />
         </Marker>
       ))}
     </MapView>
